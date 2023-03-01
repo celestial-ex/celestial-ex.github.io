a22ead14b85b71a229ee147d541f21b2f3ee81b7
celestial-ex <56469206+celestial-ex@users.noreply.github.com>
2023-03-02 02:17:39 +0800

    Add files via upload

---
+++ css/map.js
@@ -0,0 +1,65 @@
+//{content:"我的备注",title:"我的标记",imageOffset: {width:0,height:3},position:{lat:39.978501,lng:116.395394}}
+var markers = [];
+for(var i in dataArr) {
+    var dianpu = dataArr[i];
+    markers.push({
+        content: dianpu.address,
+        title: dianpu.shopName,
+        imageOffset: {width:0,height:3},
+        position:{lat:dianpu.geoLat,lng:dianpu.geoLng}
+    });
+}
+
+//创建和初始化地图函数：
+function initMap(){
+    createMap();//创建地图
+    setMapEvent();//设置地图事件
+    addMapControl();//向地图添加控件
+    addMapOverlay();//向地图添加覆盖物
+}
+function createMap(){
+    map = new BMap.Map("map");
+    map.centerAndZoom("上梅林",17);
+}
+function setMapEvent(){
+    map.enableScrollWheelZoom();
+    map.enableKeyboard();
+    map.enableDragging();
+    map.enableDoubleClickZoom()
+}
+function addClickHandler(target,window){
+    target.addEventListener("click",function(){
+        target.openInfoWindow(window);
+    });
+}
+function addMapOverlay(){
+    var markers = [
+        {content:"我的位置",title:"我的位置",imageOffset: {width:0,height:3},position:{lat:22.575000,lng:114.067000}}
+    ];
+    for(var index = 0; index < markers.length; index++ ){
+        var point = new BMap.Point(markers[index].position.lng,markers[index].position.lat);
+        var marker = new BMap.Marker(point);
+        var label = new BMap.Label(markers[index].title,{offset: new BMap.Size(25,5)});
+        var opts = {
+            width: 200,
+            title: markers[index].title,
+            enableMessage: false
+        };
+        var infoWindow = new BMap.InfoWindow(markers[index].content,opts);
+        marker.setLabel(label);
+        addClickHandler(marker,infoWindow);
+        map.addOverlay(marker);
+    };
+}
+//向地图添加控件
+function addMapControl(){
+    var scaleControl = new BMap.ScaleControl({anchor:BMAP_ANCHOR_BOTTOM_LEFT});
+    scaleControl.setUnit(BMAP_UNIT_IMPERIAL);
+    map.addControl(scaleControl);
+    var navControl = new BMap.NavigationControl({anchor:BMAP_ANCHOR_TOP_LEFT,type:BMAP_NAVIGATION_CONTROL_LARGE});
+    map.addControl(navControl);
+    var overviewControl = new BMap.OverviewMapControl({anchor:BMAP_ANCHOR_BOTTOM_RIGHT,isOpen:true});
+    map.addControl(overviewControl);
+}
+var map;
+initMap();
